a9af7a90e8c6f8db58051d33e6956bee2a8772b2
Arefin Ahmed <112863687+arefin27@users.noreply.github.com>
2025-12-02 20:10:33 +0600

    Add files via upload

---
+++ CALCULATORS/script.js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+    let display = document.getElementById("display").value;
+    document.getElementById("display").value = display.slice(0, -1);
+}
+
+function calculateResult() {
+    let expression = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(expression);
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
